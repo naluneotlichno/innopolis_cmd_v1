14cb0d81dc011abdfaf38bd4e255d336c1bf80b2
New-Beep <Lanco2701@Gmail.com>
2025-04-08 15:54:06 +0200

    add table users

--- migrations/message_chain.sql
+++ migrations/message_chain.sql
@@ -1,10 +1,18 @@
+CREATE TABLE users ( -- Создаём таблицу с пользователями
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(20) NOT NULL,
+    LastName VARCHAR(20) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT NOW()
+);
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Включаем расширение для работы с UUID, не поддерживается на сервере default
 CREATE TYPE chain_status AS ENUM ('created', 'archived'); -- создаём перечесление
 CREATE TABLE message_chains (
-id INT PRIMARY KEY, -- Уникальный идентификатор цепочки сообщений
+id INT PRIMARY KEY AUTO_INCREMENT, -- Уникальный идентификатор цепочки сообщений
 uuid UUID DEFAULT uuid_generate_v4() UNIQUE, -- Уникальный идентификатор в формате UUID
 user_id INT NOT NULL, -- Идентификатор пользователя создавшего цепочку
-creation_date DATE NOT NULL NOW(), -- Дата и время создания цепочки
+creation_at DATETIME NOT NULL NOW(), -- Дата и время создания цепочки
+updated_at DATETIME NOT NULL, -- Дата и время обновления цепочки
 status chain_status NOT NULL, -- Статус цепочки (например, active, archived)
 title VARCHAR(255), -- Заголовок цепочки
 FOREIGN KEY (user_id) REFERENCES users(id)
