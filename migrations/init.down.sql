c813532c25b39367e14c0bf6c9dbed1db6a9b58f
osagolang <lovimtutbait@yandex.ru>
2025-04-09 14:48:18 +0300

    [RS-15] Update migrations for Goose

---
+++ migrations/init.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_chain_block_links_block_id;
+DROP INDEX IF EXISTS idx_chain_block_links_chain_id;
+
+DROP TABLE IF EXISTS chain_block_links;
+DROP TABLE IF EXISTS message_blocks;
